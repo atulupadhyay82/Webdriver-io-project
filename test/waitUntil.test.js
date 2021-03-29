5d09548a524dbcabdfe002282a2a90abac4cdb16
Atulupadhyay82 <atulupadhyay82@gmail.com>
2021-03-29 13:32:40 -0400

    Adding waitUntil()

---
+++ test/waitUntil.test.js
@@ -0,0 +1,37 @@
+const { assert } = require("chai");
+
+describe("waitUntil method handle", ()=>{
+
+    it("simple wait using browser.pause()", () => {
+        
+        browser.url("https://app.hubspot.com/login");
+        const email=$('#username');
+        const password=$('#password');
+        const loginBtn= $('#loginBtn');
+        browser.pause(10000);
+        assert.isTrue(email.isDisplayed(), "Email field is not visible on the UI after 10s");
+    }),
+    it("wait until", () => {
+        
+        browser.url("https://app.hubspot.com/login");
+        const email=$('#username');
+        const password=$('#password');
+        const loginBtn= $('#loginBtn');
+        browser.waitUntil(
+            () => email.isDisplayed() === true,
+            {
+                timeout: 15000,
+                timeoutMsg: 'expected email to be visible after 15s'
+            }
+        );
+
+        const header= $('h1.private-page_title');
+        browser.waitUntil(
+            () => email.getText() === 'Sales Dashboard',
+            {
+                timeout: 6000,
+                timeoutMsg: 'expected header to be visible after 6s'
+            }
+        );  
+    })
+})
